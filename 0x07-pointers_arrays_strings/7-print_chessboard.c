264252328c906cd2b271444e626b17912e3e8229
Cyber5Tech <dhayoralabi@gmail.com>
2022-03-28 14:25:18 +0100

    fix task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * print_chessboard - prints chessboard
- * @a: double pointer
- * Return: 0
+ * print_chessboard - print in the terminal the chessboard
+ * @a: The character to print
+ *
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char(*a)[8])
 {
-	unsigned int i, j;
+	int b;
+	int c;
 
-	for (i = 0; i < 8; i++)
+	for (b = 0; b < 8; b++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-			_putchar('\n');
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+		_putchar('\n');
 	}
 }
