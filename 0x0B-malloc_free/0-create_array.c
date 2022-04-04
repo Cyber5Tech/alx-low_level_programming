09cef795d2b33678d93828c0411c3d34849f6598
Cyber5Tech <dhayoralabi@gmail.com>
2022-04-04 17:53:35 +0100

    malloc fixes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,32 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * create_array - a program that creates a dynamic memory allocation
- * filled with a specific character c .
- * @size : size of an array.
- * @c: character.
- * Return: pointer to the Array or NULL if it fails.
+ * create_array - Entry point
+ *@size: size of the array
+ *@c: character
+ * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
-char *pointer;
-unsigned int i;
+	char *array = NULL;
+	unsigned int i;
 
-if (size == 0)
-	return (NULL);
-pointer = malloc(sizeof(char) * size);
-/*
- * Allocating a specific nb of bytes on the memory
- * and returning a pointer to the allocated memory
- */
-if (pointer == NULL)
-	return (NULL);
-else
-{
-	for (i = 0; i < size; i++)
-		pointer[i] = c;
-		return (pointer);
-}
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,47 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strdup - a function to copy a string on a new allocated memory
- * area .
- * @str: string to copy
- *
- * Return: a pointer to the copied string .
+ * _strdup - Entry point
+ *@str: string we need to duplicate
+ * Return: a pointer to the duplicated string or NULL
  */
-
 char *_strdup(char *str)
 {
-char *pointeur;
-int i;
-int count = 0;
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
 
-/* in case if the given string is NULL*/
-if (str == NULL)
-	return (NULL);
-
-/* finding the length of the given string*/
-while (*(str + count))
-
-	count++;
-/*allocation the needed dynamic memory area */
-pointeur = malloc(sizeof(char) * (count + 1));
-
-
-/* to verify if the pointer , pointed on the allocated memory*/
-/* area is not NULL */
-if (pointeur == NULL)
-	return (NULL);
-
-/* initialization of i to avoid segment fault error*/
-i = 0;
-
-/* filling the allocated area with the bytes from the given string */
-while (i <= count)
-{
-	*(pointeur + i) = *(str + i);
-		i++;
-}
-
-/* return a pointer to the string */
-return (pointeur);
+	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
+	{
+		return (NULL);
+	}
+	strnew[i] = '\0';
+	return (strnew);
 }
